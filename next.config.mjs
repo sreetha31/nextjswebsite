3c8ec31b46707a917108c89a7686fd89355e9600
sreetha31 <91374082+sreetha31@users.noreply.github.com>
2024-12-05 19:06:28 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      { hostname: 'cdn.sanity.io' },
+      { hostname: 'source.unsplash.com' },
+    ],
+  },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  env: {
+    // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
+    SC_DISABLE_SPEEDY: 'false',
+  },
+  experimental: {
+    taint: true,
+  },
+}
+
+export default config
